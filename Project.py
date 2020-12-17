7c9371506bb57ac67807967d4feed2178aef9e00
NoahDBos <70241751+NoahDBos@users.noreply.github.com>
2020-12-17 14:17:33 -0800

    structured english

---
+++ Project.py
@@ -0,0 +1,7 @@
+
+"""
+Create a program that will take two inputs from the user. It will take one word first
+then take the second word. Once it has both words the program will determine if the two
+words are anagrams or not. To avoid input errors the program will also any punctions or
+spaces to be able to return a yes or no answer.
+"""
