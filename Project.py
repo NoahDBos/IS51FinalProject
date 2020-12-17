716bec4f12bc37226a2aacafc93c3fc573c3aa4d
NoahDBos <70241751+NoahDBos@users.noreply.github.com>
2020-12-17 14:24:34 -0800

    pseudocode

--- Project.py
+++ Project.py
@@ -5,3 +5,23 @@ then take the second word. Once it has both words the program will determine if 
 words are anagrams or not. To avoid input errors the program will also any punctions or
 spaces to be able to return a yes or no answer.
 """
+
+
+"""
+def main
+    string1 = input from user
+    string2 = input from user
+    if string1 and string2 are anagrams
+        print yes
+    else
+        print no
+
+def anagrams
+    string1 = make lowercase
+    string2 = make lowercase
+    gather letters1 from string1
+    gather letters2 from string2
+    sort letters1
+    sort letters2
+    return letters1 == letters2
+"""
